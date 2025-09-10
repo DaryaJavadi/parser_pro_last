ce1d4f683020b5b6c754295c9d8b7589d026c202
neyvre <darya.javadi15@gmail.com>
2025-09-10 13:12:59 +0400

    ayirmaq

--- cv_parser_pro/server.js
+++ cv_parser_pro/server.js
@@ -313,6 +313,7 @@ app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use('/api/', limiter);
+app.use('/api', apiRoutes); 
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
